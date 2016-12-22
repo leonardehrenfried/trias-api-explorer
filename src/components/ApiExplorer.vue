0a2406b97f68ec72a8de24cad3a157d6f7a51b05
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-12-22 10:51:59 +0100

    load xml from file and display it

--- src/components/ApiExplorer.vue
+++ src/components/ApiExplorer.vue
@@ -1,29 +1,25 @@
 <template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 form-inline">
+        Template:
+        <select class="form-control" v-model="templateIndex">
+          <option v-for="(template, index) in templates" :value="index">{{ template.name }}</option>
+        </select>
+      </div>
+    </div>
 
-
-<div class="container">
-
-  <div class="row">
-    <div class="col-xs-12 form-inline">
-      Template:
-      <select class="form-control" v-model="templateIndex">
-        <option v-for="(template, index) in templates" :value="index">{{ template.name }}</option>
-      </select>
+    <div class="row">
+      <div class="col-xs-4">
+        Editor:
+      </div>
+      <div class="col-xs-8">
+        <div v-if="currentTemplate">{{ currentTemplate.name }}</div>
+        <pre>{{ xmlTemplate }}</pre>
+        <div>xml output</div>
+      </div>
     </div>
   </div>
-
-  <div class="row">
-    <div class="col-xs-4">
-      Editor:
-    </div>
-    <div class="col-xs-8">
-      <div>xml editor</div>
-      <div>xml output</div>
-    </div>
-  </div>
-</div>
-
-
 </template>
 
 <script>
@@ -35,19 +31,29 @@ export default {
   data () {
     return {
       templates: [],
-      templateIndex: 0
+      templateIndex: null,
+      xmlTemplate: ''
     }
-  },
-
-  computed: {
-    currentTemplate: () => this.templates[this.templateIndex]
   },
 
   created () {
     fetch('/static/api_templates.json')
       .then((response) => response.json())
-      .then((json) => (this.templates = json))
+      .then((json) => {
+        this.templates = json
+        this.templateIndex = 0
+      })
       .catch((ex) => (console.log('parsing api_templates failed', ex)))
+  },
+
+  watch: {
+    templateIndex (newIndex) {
+      this.currentTemplate = this.templates[this.templateIndex]
+      fetch('/static/' + this.currentTemplate.template)
+          .then((response) => response.text())
+          .then((xml) => (this.xmlTemplate = xml))
+          .catch((ex) => (console.log(`loading of xml template ${this.currentTemplate.template} failed`)))
+    }
   }
 }
 </script>
