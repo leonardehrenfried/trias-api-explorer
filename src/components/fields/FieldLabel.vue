215cc5db518e916ba16ff4aa007b762417dc5a7b
Fabio Anderegg <fabio.anderegg@liip.ch>
2017-01-12 14:33:44 +0100

    label: use triple comparision operator

--- src/components/fields/FieldLabel.vue
+++ src/components/fields/FieldLabel.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label :for="variable.name" class="control-label">{{ variable.label }}{{ variable.required == true ? '*' : '' }}</label>
+    <label :for="variable.name" class="control-label">{{ variable.label }}{{ variable.required === true ? '*' : '' }}</label>
     <div v-if="variable.description">{{ variable.description }}</div>
   </div>
 </template>
